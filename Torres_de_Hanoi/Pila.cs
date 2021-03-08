ba74042d50d0ea1c98b2be46b006df01f9766218
poquet <dpoqram@epsg.upv.es>
2021-03-08 16:02:14 +0100

    Primer commit de la clase disco y pila

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -7,9 +7,7 @@ using System.Threading.Tasks;
 namespace Torres_de_Hanoi
 {
     class Pila
-    {
-        public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
+    {   /* TODO: Elegir tipo de Top
         public int Top { get; set; }
         public String Top { get; set; }        
         */
@@ -18,20 +16,33 @@ namespace Torres_de_Hanoi
         public List<Disco> Elementos { get; set; }
         */
 
+        //Para representar la cantidad de discos que hay en el palo.
+        public int Size { get; set; }
+
+        //Para representar el disco que está en la parte superior del palo.
+        public int Top { get; set; }
+
+        //Para representar el conjunto de discos que hay en ese palo.
+        public List<Disco> Elementos { get; set; }
+
+
+
         /* TODO: Implementar métodos */
         public Pila()
         {
-
+            this.Elementos = new List<Disco>();
         }
 
+        //que permitirá colocar un disco en la pila de discos
         public void push(Disco d)
         {
-
+            this.Elementos.Add(d);
         }
 
+        //que permitirá extraer el disco de la parte superior de la pila
         public Disco pop()
         {
-            return null;
+            return this.Elementos[(this.Elementos.Count - 1)];
         }                
 
         public bool isEmpty()
